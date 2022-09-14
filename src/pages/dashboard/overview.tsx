1fa5ab44cb94da10bc025a5b8291680897b1f76a
Tembero <silver.teemu@gmail.com>
2022-09-14 18:31:38 +0300

    Overview change name

--- src/pages/dashboard/overview.tsx
+++ src/pages/dashboard/overview.tsx
@@ -11,23 +11,38 @@ import DashboardLayout from "./Layout";
 
 export default function DashboardOverviewPage() {
 
-    const [ serverMeta, setServerMeta ] = useState<ServerMeta | null>(null)
+    const [serverMeta, setServerMeta] = useState<ServerMeta | null>(null)
+    const [serverName, setServerName] = useState("Loading...")
 
     const { tag } = useParams() as { tag: string };
 
     useEffect(() => {
-        fetchServer(tag).then(meta => setServerMeta(meta));
+        fetchServer(tag).then(meta => {
+            setServerMeta(meta);
+            setServerName(meta.name);
+        });
     }, []);
 
     return (
         <DashboardLayout pageName={"Overview"}>
-            <h1>{serverMeta ? serverMeta.name : "Loading..."}</h1>
-                <StatusControl tag={tag}/>
-                <div className={styles.tiles}>
-                    <ValueTile valueName={"Processor"} value={"56%"} percentage={0.56}/>
-                    <ValueTile valueName={"Memory"} value={"79%"} percentage={0.79}/>
-                    <ValueTile valueName={"Players"} value={"4/20"} percentage={4/20}/>
+            <form onSubmit={e => {
+                // TODO: fetch
+                e.preventDefault()
+            }}>
+                <input className={styles.headerInput} value={serverName} onChange={e => setServerName(e.target.value)} maxLength={20} />
+                <div className={styles.editSaveCancel} style={{
+                    display: serverMeta?.name === undefined || serverMeta?.name === serverName ? "none" : "flex"
+                }}>
+                    <button className={styles.editCancel} onClick={() => setServerName(serverMeta?.name || "")}>Cancel</button>
+                    <button>Save</button>
                 </div>
+            </form>
+            <StatusControl tag={tag} />
+            <div className={styles.tiles}>
+                <ValueTile valueName={"Processor"} value={"56%"} percentage={0.56} />
+                <ValueTile valueName={"Memory"} value={"79%"} percentage={0.79} />
+                <ValueTile valueName={"Players"} value={"4/20"} percentage={4 / 20} />
+            </div>
         </DashboardLayout>
     )
 }
